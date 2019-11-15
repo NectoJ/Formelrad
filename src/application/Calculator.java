95ba9490ca9f44f15360b70c42464c5a806fe2ea
raphael.suter <raphael.suter@gibwintra.ch>
2019-11-15 13:22:19 +0100

    Implement calculation

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -45,7 +45,25 @@ public class Calculator {
 	}
 
 	public void calculate() {
-		
+		if (leistung != 0.0 && spannung != 0.0) {
+			strom = leistung / spannung;
+			widerstand = spannung * spannung / leistung;
+		} else if (leistung != 0.0 && strom != 0.0) {
+			spannung = leistung / strom;
+			widerstand = leistung / (strom * strom);
+		} else if (leistung != 0.0 && widerstand != 0.0) {
+			spannung = Math.sqrt(leistung * widerstand);
+			strom = Math.sqrt(leistung / widerstand);
+		} else if (spannung != 0.0 && widerstand != 0.0) {
+			leistung = spannung * spannung / widerstand;
+			strom = spannung / widerstand;
+		} else if (spannung != 0.0 && strom != 0.0) {
+			leistung = spannung * strom;
+			widerstand = spannung / strom;
+		} else if (widerstand != 0.0 && strom != 0.0) {
+			leistung = widerstand * strom * strom;
+			spannung = strom * widerstand;
+		}
 	}
 
 	public double pAusUundI(double u, double i) {
